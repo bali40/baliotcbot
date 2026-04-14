edc1b755b8bc968fa1b59a7b50c299d8919f52f2
bali40 <s.wozny@bigan.pl>
2026-04-14 21:31:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -233,5 +233,5 @@ def telegram_webhook():
     return jsonify({"ok": True})
 
 
-if _name_ == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
